534a25de6fe022827fb4b9dd4c7bcfedd082c8a6
Tunico <132064093+Thkny@users.noreply.github.com>
2024-04-03 18:46:15 -0300

    uns testes apenas

---
+++ Projetinho/Assets/NewScripts/Testes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testes : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        int var = 10;
+        float peso = 61.08f;
+        double medida = 9.99999999;
+        bool ativado = false;
+        string texto = "fala boy";
+
+
+        Debug.Log("inteiro: " + var);
+        Debug.Log("Peso: " + peso);
+        Debug.Log("Medida: " + medida);
+        Debug.Log("Ativado: " + ativado);
+        Debug.Log("Texto: " + texto);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
