5bde2a83630ae7afbcf90a532c81a1b7c5664b1b
Tunico <132064093+Thkny@users.noreply.github.com>
2024-04-17 19:32:21 -0300

    tbm

---
+++ Projetinho/Assets/NewScripts/Teste11.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste11 : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        int[] numbers = { 1, 2, 3, 4, 89, 74, 58 };
+        Debug.Log(numbers);
+        Debug.Log(numbers[3]);
+        Debug.Log(numbers[6]);
+
+        float[] grades = { 9.2f, 5.5f, 7.8f, 9.4f, 9.8f };
+        foreach (float grade in grades) {
+            Debug.Log(grade);
+        }
+
+        int[] values = new int[5];
+        //values[0] = 3;
+        //values[1] = 4;
+
+        //Debug.Log(values[0]);
+
+        for (int i = 0; i < values.Length; i++) {
+            numbers[i] = i;
+            Debug.Log(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
